bf1b7f32cda497a2d9337502f266d78844c154a6
alsmirnoff <alex-sm-91@mail.ru>
2026-02-24 18:51:09 +0300

    lesson 1-3

---
+++ src/app.js
@@ -0,0 +1,6 @@
+var test_string = 'it works!';
+var test_number = 123;
+var test_boolean = true;
+alert(test_string);
+alert(test_number);
+alert(test_boolean);
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+let test_string: string = 'it works!';
+let test_number: number = 123;
+let test_boolean: boolean = true;
+alert(test_string);
+alert(test_number);
+alert(test_boolean);
